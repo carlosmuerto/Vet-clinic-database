5ba6b240af210ebd77e99c005220a791fb141e3f
Carlos Vivas <carlosmuerto@gmail.com>
2022-10-19 17:55:39 -0400

    Add species foreign keys

--- data.sql
+++ data.sql
@@ -18,3 +18,38 @@ INSERT INTO public.animals(
 	( 'Boarmon', '2005-06-7', 7, TRUE ,20.4 ),
 	( 'Blossom', '1998-10-13', 3, TRUE ,17 ),
 	( 'Ditto', '2022-05-14', 4, TRUE ,22 );
+
+BEGIN;
+
+INSERT INTO public.owners(
+     full_name,
+     age
+) VALUES
+	('Sam Smith' ,34),
+	('Jennifer Orwell' ,19),
+	('Bob' ,45),
+	('Melody Pond' ,77),
+	('Dean Winchester' ,14),
+	('Jodie Whittaker' ,38);
+
+INSERT INTO public.species(
+     name
+) VALUES
+    ('Pokemon'),
+    ('Digimon');
+
+COMMIT;
+
+BEGIN;
+
+UPDATE public.animals
+	SET species_id = (SELECT id FROM species WHERE name = 'Digimon')
+WHERE name LIKE '%mon';
+
+SELECT * from animals;
+
+UPDATE public.animals
+	SET species_id = (SELECT id FROM species WHERE name = 'Pokemon')
+WHERE species_id is NULL;
+
+COMMIT;
